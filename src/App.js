d9a14b96f963a2aed17afa9d50915d45e7e87563
kenny <kenny.kwyiu@gmail.com>
2024-01-21 09:56:04 +0800

    93-added FormSplitBill

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
         <FormAddFriend />
         <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -76,6 +77,30 @@ function FormAddFriend() {
       <input type="text" />
 
       <Button>Select</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>🤑 Bill value</label>
+      <input type="text" />
+
+      <label>🕴️ Your expense</label>
+      <input type="text" />
+
+      <label>🤼 X's expense</label>
+      <input type="text" disabled />
+
+      <label>👻 Who is paying the bill</label>
+      <select>
+        <option value={"user"}>You</option>
+        <option value={"friend"}>You</option>
+      </select>
+
+      <Button>Split Bill</Button>
     </form>
   );
 }
