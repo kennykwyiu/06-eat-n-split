d7f73e0196c079260d767c4830da7c2094af50c0
kenny <kenny.kwyiu@gmail.com>
2024-02-06 22:36:46 +0800

    133-using the key prop to fix  our eat-n-split-app

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
-          key={selectedFriend.id}
+          key={selectedFriend.name}
         />
       )}
     </div>
